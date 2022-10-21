b2668f4cc47ee3aa040c7cd8c0177cc242754276
Staryy-Dobryy <lfcega1@gmail.com>
2022-10-21 13:05:54 +0300

    Modified

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,25 +1,28 @@
-firstNumber = int(input("Enter first number: "))
-secondNumber = int(input("Enter second number: "))
-operation = input("Enter operation: sum/diff/multiple/divide/power/radical\n")
-if operation == "sum":
-    print ("Result:" + str(firstNumber + secondNumber))
-elif operation == "diff":
-    print ("Result:" + str(firstNumber - secondNumber))
-elif operation == "multiple":
-    print ("Result:" + str(firstNumber * secondNumber))
-elif operation == "divide":
-    if secondNumber == 0:
-        print ("Invalid second number")     #cannot divide by zero
+try:
+    firstNumber = int(input("Enter first number: "))
+    secondNumber = int(input("Enter second number: "))
+    operation = input("Enter operation: sum/diff/multiple/divide/power/radical\n")
+    if operation == "sum":
+        print ("Result:" + str(firstNumber + secondNumber))
+    elif operation == "diff":
+        print ("Result:" + str(firstNumber - secondNumber))
+    elif operation == "multiple":
+        print ("Result:" + str(firstNumber * secondNumber))
+    elif operation == "divide":
+        if secondNumber == 0:
+            print ("Invalid second number")     #cannot divide by zero
+        else:
+            print ("Result:" + str(firstNumber / secondNumber))
+    elif operation == "power":
+        print ("Result:" + str(firstNumber ** secondNumber))
+    elif operation == "radical":
+        if firstNumber < 0:
+            print ("Invalid first number")      #radical expression cannot be negative
+        elif secondNumber == 0:
+            print ("Invalid second number")     #root exponent cannot be zero
+        else:
+            print ("Result: " + str(firstNumber ** 1/secondNumber))
     else:
-        print ("Result:" + str(firstNumber / secondNumber))
-elif operation == "power":
-    print ("Result:" + str(firstNumber ** secondNumber))
-elif operation == "radical":
-    if firstNumber < 0:
-        print ("Invalid first number")      #radical expression cannot be negative
-    elif secondNumber == 0:
-        print ("Invalid second number")     #root exponent cannot be zero
-    else:
-        print ("Result:" + str(firstNumber ** 1/secondNumber))
-else:
-    print ("Invalid operation")
+        print ("Invalid operation")
+except:
+    print ("Invalid value")
