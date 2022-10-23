c1596994fdc0b1536fe3966cb0fa50e0a6e82598
Staryy-Dobryy <lfcega1@gmail.com>
2022-10-23 14:53:32 +0300

    Changed

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,27 +1,17 @@
 try:
-    a = int(input("Enter first coefficient: "))
-    b = int(input("Enter second coefficient: "))
-    c = int(input("Enter third coefficient: "))
-    if a < 0: quit();
+    coefficientA = int(input("Enter first coefficient: "))
+    coefficientB = int(input("Enter second coefficient: "))
+    coefficientC = int(input("Enter third coefficient: "))
+    if coefficientA < 0: quit();
     print ("Result: " , end="")
-    if a != 0 and b != 0 and c != 0:
-        d = b ** 2 - 4 * a * c
-        if d < 0: quit()
-        if d > 0:
-            x1 = (-b+d**(1/2))/(2*a)
-            x2 = (-b-d**(1/2))/(2*a)
-            print ("x1=" + str(x1) + " |x2=" + str(x2))
-        elif d == 0: 
-            x = -b/(2*a)
-            print ("x=" + str(x))
-    elif a != 0 and b != 0:
-        x1 = 0
-        x2 = -b/a
+    d = coefficientB ** 2 - 4 * coefficientA * coefficientC
+    if d < 0: quit()
+    if d > 0:
+        x1 = (-coefficientB+d**(1/2))/(2*coefficientA)
+        x2 = (-coefficientB-d**(1/2))/(2*coefficientA)
         print ("x1=" + str(x1) + " |x2=" + str(x2))
-    elif a != 0 and c != 0:
-        if c > 0: quit()
-        x1 = (-c/a)**(1/2)
-        x2 = -((-c/a)**(1/2))
-        print ("x1=" + str(x1) + " |x2=" + str(x2))
+    elif d == 0: 
+        x = -coefficientB/(2*coefficientA)
+        print ("x=" + str(x))
 except:
     print ("Invalid value")
