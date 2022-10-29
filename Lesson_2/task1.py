58d192fd40fc393335077d2ceea595b9bbf20b88
Staryy-Dobryy <lfcega1@gmail.com>
2022-10-29 18:36:50 +0300

    Optimized

--- Lesson_2/task1.py
+++ Lesson_2/task1.py
@@ -1,17 +1,11 @@
 stringArray = list(''.join(sorted(input("Enter string: "))))
 arrayOfAmount, arrayOfSymbol = [], []
-for index in range(len(stringArray)):
-    amount = 0
-    temp = stringArray[index]
-    if temp != stringArray[index - 1]:
-        for ID in range(len(stringArray)):  
-            if stringArray[ID] == temp: amount += 1
-            if ID + 1 == len(stringArray):
-                print(stringArray[index], "-", amount)
-                arrayOfSymbol.append(stringArray[index])
-                arrayOfAmount.append(amount)
+setStringArray = sorted(set(stringArray))
+for i in range(len(setStringArray)):
+    arrayOfAmount.append(stringArray.count(setStringArray[i]))
+    arrayOfSymbol.append(setStringArray[i])
+    print(setStringArray[i], "-", arrayOfAmount[i])
 print()
-temp = 0
 for i in range(len(arrayOfAmount)):
     for j in range(len(arrayOfAmount)):
         if arrayOfAmount[i] > arrayOfAmount[j]:
