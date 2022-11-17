64805b7a47342372e210d3d6619d356fcb6582df
Staryy-Dobryy <lfcega1@gmail.com>
2022-11-17 21:29:35 +0200

    Modified

--- We know/bank_card.py
+++ We know/bank_card.py
@@ -1,13 +1,31 @@
-numCard = list(input("Enter number: "))
-amexNumber = ['34', '37']
-mastercardNumber = ['51', '52', '53', '54', '55']
-firstTwo = numCard[0] + numCard[1]
-lenght = len(numCard)
-if firstTwo in amexNumber and lenght == 15:
-    print("AMEX")
-elif firstTwo in mastercardNumber and lenght == 16:
-    print("MASTERCARD")
-elif numCard[0] == "4" and lenght == 13 or numCard[0] == "4" and lenght == 16:
-    print("VISA")
-else:
-    print("INVALID")
+def validCheck(number):
+    index = len(number) - 2
+    validSum = 0
+    while (index >= -1):
+        temp = int(number[index]) * 2
+        validSum += int(number[index + 1])
+        if index == -1: break
+        for item in str(temp): validSum += int(item)
+        index -= 2
+    if str(validSum)[-1] == "0": print("VALID NUMBER")
+    else: print("INVALID NUMBER")
+
+def checkCard(numCard):
+    amexNumber = ['34', '37']
+    mastercardNumber = ['51', '52', '53', '54', '55']
+    firstTwo = numCard[0] + numCard[1]
+    lenght = len(numCard)
+    if firstTwo in amexNumber and lenght == 15:
+        print("AMEX")
+        validCheck(numCard)
+    elif firstTwo in mastercardNumber and lenght == 16:
+        print("MASTERCARD")
+        validCheck(numCard)
+    elif numCard[0] == "4" and lenght == 13 or numCard[0] == "4" and lenght == 16:
+        print("VISA")
+        validCheck(numCard)
+    else:
+        print("INVALID")
+
+checkCard(input("Enter number: "))
+    
